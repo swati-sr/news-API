45fa5b448b9473644487d6f54cef2ace33590fb4
swati-sr <85557143+swati-sr@users.noreply.github.com>
2021-06-18 12:06:27 +0530

    Delete feed.js

--- feed.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const feedController = require('../controllers/feed');
-
-const app = express();
-const router = express.Router();
-
-router.get('/getnews', feedController.getPosts);
-
-module.exports = router;
