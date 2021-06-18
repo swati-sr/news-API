d9a96cf9c4d1af0843e897dc70e2e0fbc3c3e0ef
swati-sr <85557143+swati-sr@users.noreply.github.com>
2021-06-18 12:07:37 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,59 @@
+exports.getPosts = (req, res) => {
+    res.json({
+        news : [
+            {
+                "guid": "f7dc6c18-e597-4bcb-848e-0f898f9009c4",
+                "title": "Re: A Mediterranean escape from the metropolis at Park Hyatt Dubai",
+                "link": "https://www.arabianbusiness.com/travel-hospitality/460163-re-mediterranean-escape-from-the-metropolis-at-park-hyatt-dubai",
+                "publishedDate": 1615453966000,
+                "newsCount": 1,
+                "description": "Bright whitewashed walls, wooden balconies, Santorini blue rooves and lush green gardens rolling up to the path by the water's edge provide the ideal haven to stop the clock.",
+                "sentiment": 0,
+                "source": "Arabian Business",
+                "instrument": {"n": "METROPOLIS HEALTHCARE LTD",
+                "e": "NSE_EQ","s": "METROPOLIS","t": "9581"
+                }
+            },
+            {
+                "guid": "f7dc6c18-e597-4bcb-848e-0f898f9009c4",
+                "title": "Re: A Mediterranean escape from the metropolis at Park Hyatt Dubai",
+                "link": "https://www.arabianbusiness.com/travel-hospitality/460163-re-mediterranean-escape-from-the-metropolis-at-park-hyatt-dubai",
+                "publishedDate": 1615453966000,
+                "newsCount": 1,
+                "description": "Bright whitewashed walls, wooden balconies, Santorini blue rooves and lush green gardens rolling up to the path by the water's edge provide the ideal haven to stop the clock.",
+                "sentiment": 0,
+                "source": "Arabian Business",
+                "instrument": {"n": "METROPOLIS HEALTHCARE LTD",
+                "e": "NSE_EQ","s": "METROPOLIS","t": "9581"
+                }
+            },
+            {
+                "guid": "f7dc6c18-e597-4bcb-848e-0f898f9009c4",
+                "title": "Re: A Mediterranean escape from the metropolis at Park Hyatt Dubai",
+                "link": "https://www.arabianbusiness.com/travel-hospitality/460163-re-mediterranean-escape-from-the-metropolis-at-park-hyatt-dubai",
+                "publishedDate": 1615453966000,
+                "newsCount": 1,
+                "description": "Bright whitewashed walls, wooden balconies, Santorini blue rooves and lush green gardens rolling up to the path by the water's edge provide the ideal haven to stop the clock.",
+                "sentiment": 0,
+                "source": "Arabian Business",
+                "instrument": {"n": "METROPOLIS HEALTHCARE LTD",
+                "e": "NSE_EQ","s": "METROPOLIS","t": "9581"
+                }
+            },
+            {
+                "guid": "f7dc6c18-e597-4bcb-848e-0f898f9009c4",
+                "title": "Re: A Mediterranean escape from the metropolis at Park Hyatt Dubai",
+                "link": "https://www.arabianbusiness.com/travel-hospitality/460163-re-mediterranean-escape-from-the-metropolis-at-park-hyatt-dubai",
+                "publishedDate": 1615453966000,
+                "newsCount": 1,
+                "description": "Bright whitewashed walls, wooden balconies, Santorini blue rooves and lush green gardens rolling up to the path by the water's edge provide the ideal haven to stop the clock.",
+                "sentiment": 0,
+                "source": "Arabian Business",
+                "instrument": {"n": "METROPOLIS HEALTHCARE LTD",
+                "e": "NSE_EQ","s": "METROPOLIS","t": "9581"
+                }
+            },
+
+        ]
+    });
+};
